c1837fd9d1d6d8dc737b0e41361e58616c25da6d
Ops <krzysztof.papciak@hotmail.com>
2019-12-08 18:42:06 +0100

    Email
    Create value object

---
+++ GroceryExpressCart/GroceryExpressCart.Core/ValueObjects/Email.cs
@@ -0,0 +1,30 @@
+﻿using GroceryExpressCart.Common.Entity;
+using GroceryExpressCart.Common.Extension;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroceryExpressCart.Core.ValueObjects
+{
+    public class Email : ValueObject<Email>
+    {
+        public string Value { get; }
+        private Email(string email) => Value = email;
+
+        public static Email Create(string email)
+        {
+            if (email.IsEmpty())
+            {//TODO
+            }
+            return new Email(email);
+        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            throw new NotImplementedException();
+        }
+        public override int GetHashCode()
+        {
+            return HashCodeGenerator.Of(Value);
+        }
+    }
+}
