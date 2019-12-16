df2afc9c18070dab7fd36ff99efcf083eed5e1cc
Ops <krzysztof.papciak@hotmail.com>
2019-12-16 20:11:21 +0100

    Fix Events handling

--- GroceryExpressCart/GroceryExpressCart.Core/Domain/MemberShip.cs
+++ GroceryExpressCart/GroceryExpressCart.Core/Domain/MemberShip.cs
@@ -1,5 +1,7 @@
 ﻿using GroceryExpressCart.Common.Entity;
+using GroceryExpressCart.Core.Events;
 using GroceryExpressCart.Core.ValueObject;
+using System;
 using System.Collections.Generic;
 
 namespace GroceryExpressCart.Core.Domain
@@ -8,7 +10,7 @@ namespace GroceryExpressCart.Core.Domain
     {
         public Email Email { get; }
         public Login Login { get; }
-        public Password Password { get; set; }
+        public Password Password { get; }
         public ICollection<Order> Orders { get; }
         private MemberShip() { }
         public MemberShip(Email email, Login login, Password password) : this()
@@ -16,6 +18,13 @@ namespace GroceryExpressCart.Core.Domain
             Email = email;
             Login = login;
             Password = password;
+            AddDomainEvent(new CreatedAccountEvent(DateTime.Now, nameof(EventMessage.CREATED_USER)));
+        }
+        public MemberShip(Login login, Password password)
+        {
+            Login = login;
+            Password = password;
+            AddDomainEvent(new LoginAccountEvent(DateTime.Now, nameof(EventMessage.LOGIN_USER)));
         }
         public override int GetHashCode() => HashCodeGenerator.Of(Login).And(Password).And(Email);
     }
